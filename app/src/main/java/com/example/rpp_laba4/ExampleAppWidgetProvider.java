2feb489bbac58d20ee0b12d0887bf23d2f3c672c
BadKrog <anton.zubreev@mail.ru>
2020-04-13 02:00:21 +0300

    Finish project

--- app/src/main/java/com/example/rpp_laba4/ExampleAppWidgetProvider.java
+++ app/src/main/java/com/example/rpp_laba4/ExampleAppWidgetProvider.java
@@ -16,6 +16,10 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
 
     private static int day;
 
+    static {
+        day = -1;
+    }
+
     public static void info(int days){
         day = days;
     }
@@ -26,13 +30,10 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
         // Perform this loop procedure for each App Widget that belongs to this provider
         for (int i=0; i<N; i++) {
 
-            Log.d("MyTag", "I in start update");
-
             int appWidgetId = appWidgetIds[i];
 
             // Create an Intent to launch ExampleActivity
             Intent intent = new Intent(context, MainActivity.class);
-
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
             // Get the layout for the App Widget and attach an on-click listener
@@ -40,11 +41,6 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_appwidget);
             views.setOnClickPendingIntent(R.id.text_widget, pendingIntent);
             views.setTextViewText(R.id.text_widget, ""+day);
-
-            /*View view = new View(context);
-            TextView textDay = (TextView) view.findViewById(R.id.text_widget);
-            textDay.setText(""+days);*/
-            Log.d("MyTag", "Дней осталось:"+day);
 
 
             // Tell the AppWidgetManager to perform an update on the current app widget
@@ -55,5 +51,11 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
 
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         update(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Log.d("MyTag", "Ну здесь то работает");
     }
 }
--- app/src/main/java/com/example/rpp_laba4/MainActivity.java
+++ app/src/main/java/com/example/rpp_laba4/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.rpp_laba4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CalendarView;
@@ -18,19 +26,34 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    // View
     CalendarView calendar;
+    TextView textDay;
+
+    // Все связанное со временем
     Calendar currantDate;
     Calendar changeDate;
-    TextView textDay;
-    Intent intent;
+
+    // Widget
     AppWidgetManager appWidgetManager;
     int appWidgetIds[];
+
+    // Alarm
+    AlarmManager alarmManager;
+    PendingIntent pendingIntent;
+    Intent alarmIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Создаем и настраиваем alarm
+        alarmIntent = new Intent(this, CallDataSend.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        // Получаем информацию и ссылки на widget
         appWidgetManager = AppWidgetManager.getInstance(this);
         appWidgetIds = appWidgetManager.getAppWidgetIds(
                                         new ComponentName
@@ -54,28 +77,38 @@ public class MainActivity extends AppCompatActivity {
         // Устанавливаем минимальную дату для выбора
         calendar.setMinDate(calendar.getDate());
 
-        intent = new Intent(this, ExampleAppWidgetProvider.class);
-
         // Устанавливаем слушателя на выбор даты в календаре
         calendar.setOnDateChangeListener(new OnDateChangeListener(){
             // Описываем метод выбора даты в календаре:
             @Override
             public void onSelectedDayChange(CalendarView view, int year,int month, int dayOfMonth) {
 
+                // Вычисляем разницу в днях
                 changeDate.set(year, month, dayOfMonth);
                 long millisecond = changeDate.getTimeInMillis() - currantDate.getTimeInMillis();
                 int days = (int)(millisecond/(24*60*60*1000));
+
+                // Устанавливаем значение в View
                 textDay.setText("Осталось дней: "+days);
 
+                // Передаем информацию в Widget
                 ExampleAppWidgetProvider.info(days);
                 ExampleAppWidgetProvider.update(MainActivity.this, appWidgetManager, appWidgetIds);
 
-                // При выборе любой даты отображаем Toast сообщение с данными о выбранной дате (Год, Месяц, День):
-                Toast.makeText(getApplicationContext(),
-                        "Год: " + year + "\n" +
-                                "Месяц: " + month + "\n" +
-                                "День: " + dayOfMonth,
-                        Toast.LENGTH_SHORT).show();
+                // Включаем alarm, сработает через 5 секунд после нажатия
+                alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+5000, pendingIntent);
+
+                // Программа, которая будет показывать уведомление в 9 часов утра выбранного дня
+                //alarmManager.set(AlarmManager.RTC_WAKEUP, changeDate.getTimeInMillis()+(60*60*1000+(51*60*1000)), pendingIntent);
+
+                // Возможность установить собственное время вручную
+                /*Calendar myTest = Calendar.getInstance();
+                myTest.set(Calendar.YEAR, 2020);
+                myTest.set(Calendar.MONTH, 3);
+                myTest.set(Calendar.DAY_OF_MONTH, 13);
+                myTest.set(Calendar.HOUR_OF_DAY, 1);
+                myTest.set(Calendar.MINUTE, 58);
+                alarmManager.set(AlarmManager.RTC_WAKEUP, myTest.getTimeInMillis(), pendingIntent);*/
             }});
     }
 }
